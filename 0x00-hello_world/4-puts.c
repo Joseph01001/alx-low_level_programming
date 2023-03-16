e8ed7f2255ce77207da425f15bc578a1da765b71
Joseph01001 <fadhilirandu.fr@gmail.com>
2023-03-16 13:16:16 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+/**
+ *main- function is voided
+*/
+int main(void)
+{
+	/**
+	*puts- outputs "Programming is like building a multilingual puzzle"
+	*/
+	puts("Programming is like building a multilingual puzzle\n");
+	/**
+	*return- value is 0
+	*/
+	return (0);
+}
