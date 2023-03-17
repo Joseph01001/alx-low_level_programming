a972a65ad6f4a90b8f16485f1fcdbda7a41d5b60
Joseph01001 <fadhilirandu.fr@gmail.com>
2023-03-17 13:27:39 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,13 +10,9 @@ int main(void)
 	int letter;
 
 	for (letter = 'a'; letter <= 'z'; letter)
-	{
 		putchar(letter);
-	}
 	for (ch = 'A'; letter <= 'Z'; letter++)
-	{
 		putchar(letter);
-	}
 	putchar('\n');
 	return (0);
 }
