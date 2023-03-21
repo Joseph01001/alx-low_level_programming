c2549914b3bb49ea1f52f5661a1aa2a84aa95837
Joseph01001 <fadhilirandu.fr@gmail.com>
2023-03-21 14:38:56 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_islower - runtime begins here
+ *@c: characters to be checked
+ *
+ *Description: checks for lowercase characters
+ *Return: returns 1/0
+ */
+int _islower(int c)
+
+{
+	if (c == 99)
+		return (1);
+	else
+		return (0);
+	
+}
