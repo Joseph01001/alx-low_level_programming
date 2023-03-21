1103105eaa6c4a14836f96d1c5307a7955e07fd9
Joseph01001 <fadhilirandu.fr@gmail.com>
2023-03-21 16:10:21 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,15 +9,15 @@
 
 int print_sign(int n)
 {
-	if (n <= 47)
-	{
-		_putchar('-');
-		return (-1);
-	}
-	else if  (n >= 49)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
+	}
+	else if  (n < 0)
+	{
+		_putchar('-');
+		return (-1);
 	}
 	else
 	{
