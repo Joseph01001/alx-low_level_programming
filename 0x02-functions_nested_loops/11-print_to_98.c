364e07e12ff87bcd523782d3d4622df1aca2ee29
Joseph01001 <fadhilirandu.fr@gmail.com>
2023-03-22 17:07:44 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,86 +10,33 @@
 void print_to_98(int n)
 {
 	int a;
-	int b;
-	int c;
-	int d;
 
-	if (n <= 98)
+	if (n >= 0 &&n <= 98)
 	{
-	for (; n <= 98 ; n++)
-	{
-		if (n == 0)
+		for (a = n; a <= 98 ; a++)
 		{
-			_putchar(n + '0');
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", a);
 		}
-		else if (n > 9)
-		{
-			a = n % 10;
-			b = n / 10;
-			_putchar(b + '0');
-			_putchar(a + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-		else
-		{
-			_putchar(n + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-		if (n < 0)
-		{
-			d = -(n);
-			_putchar('-');
-			_putchar(d + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-		else if (n < -9)
-		{
-			_putchar('-');
-			_putchar((d / 10) + '0');
-			_putchar((d % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-		else if (n < -99)
-		{
-			_putchar('-');
-			_putchar((d / 100) + '0');
-			_putchar((n - (d / 100)) + '0');
-			_putchar((d % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
+		printf("\n");
 	}
-	_putchar('\n');
-	}
-	else if (n >= 98)
+	else if (n < 0)
 	{
-		for (; n >= 98 ; n--)
+		for (a = n ; a <= 98 ; a++)
 		{
-			c = n / 100;
-			b = (n - (c * 100)) / 10;
-			a = n % 10;
-			if (n > 99)
-			{
-			_putchar(c + '0');
-			_putchar(b + '0');
-			_putchar(a + '0');
-			_putchar(',');
-			_putchar(' ');
-			}
-			else
-			{
-				_putchar(b + '0');
-				_putchar(a + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
+			printf("%d, ", a);
 		}
-		_putchar('\n');
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (a = n ; a >= 98 ; a--)
+		{
+			printf("%d. ", a);
+		}
+		printf("\n");
+	}
+	else if (n == 98)
+	{
+		printf("%d, ", n);
 	}
 }
