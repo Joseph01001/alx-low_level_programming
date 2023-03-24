c6ffb8093eab47476ce1163ffe3db6dac77c6860
Joseph01001 <fadhilirandu.fr@gmail.com>
2023-03-24 16:32:22 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ *main - runtime starts here
+ *
+ *Description: for multiples of three print Fizz
+ * for the multiples of five print Buzz
+ * for numbers which are multiples of both three and five print FizzBuzz
+ *Return: return is always 0
+ */
+int main(void)
+{
+	int range;
+
+	for (range = 1 ; range <= 100 ; range++)
+	{
+		if (range % 3 == 0 && range % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+		else if (range % 5 == 0 && range % 3 != 0)
+		{
+			printf("Buzz ");
+		}
+		else if (range % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", range);
+		}
+	}
+	printf("\n");
+	return (0);
+}
