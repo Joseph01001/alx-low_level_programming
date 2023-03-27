d182e2089cd1e084df82cb985a5f05268f0f78fc
Joseph01001 <fadhilirandu.fr@gmail.com>
2023-03-27 09:36:13 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *reset_to_98 - runtime begines here
+ *@n: conditional parameter
+ *
+ *Desscription: function that updates pointer to 98
+ *Return: always 0 (success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+contains pointers, arrays and strings tasks
