bc0c5eb91cc31ba834fb3b200b1ab17162744813
Joseph01001 <fadhilirandu.fr@gmail.com>
2023-03-27 09:47:05 -0700

    Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *swap_int - runtime starts here
+ *@a: conditional parameter
+ *@b: conditional parameter
+ *
+ *Description: functon that swaps two assigned numbers
+ *Return: always 0 (successs), voided in this case
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
